05cb432f006e738007eaefe0b9d9634c8134eadb
cpjjpeters <cpjjpeters@gmail.com>
2020-03-13 15:39:17 +0100

    first commit

---
+++ src/main/java/be/belfius/sbjmscrud/SbJmsCrudApplication.java
@@ -0,0 +1,13 @@
+package be.belfius.sbjmscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJmsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJmsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/belfius/sbjmscrud/SbJmsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package be.belfius.sbjmscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJmsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
